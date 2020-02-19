ee52b11d4e7bc624d22a47ed667930bad641b7a4
junichi.enomoto <junichi.enomoto@tokyo.supersoftware.co.jp>
2020-02-19 16:19:09 +0900

    first commit

--- ferrari.php
+++ ferrari.php
@@ -8,10 +8,33 @@
             $this->price = 1500;
             $this->capacity = 2;
             $this->speed = 120;
+            $this->height = 120;
+            $this->liftupheight = $this->height + 0.4;
+
+        }
+
+        public function liftup($height,$liftupheight,$liftupspeed){
+            $msg =  "リフトアップ前の車高は".$height."cmです。フロント車高を40mmリフトアップしました。車高は".$liftupheight."cmです。加速スピードは".$liftupspeed."km/hに減速します。";
+            return $msg;
+        }
+    
+        public function liftdown($height, $speed){
+            $msg = "リフトダウンを行いました。車高は".$height."cmに戻ります。加速スピードは".$speed."km/hに戻ります。";
+            return $msg;
         }
     }
 
     $ferrari = new Ferrari();
-    echo $ferrari->accelerator();
-    echo $ferrari->brake();
+    $height = $ferrari->height;
+    $speed = $ferrari->speed;
+    $liftupheight = $ferrari->liftupheight;
+    $liftupspeed = $ferrari->speed * 0.8;
+
+    echo ($ferrari->liftup($height, $liftupheight, $liftupspeed));
+
+    if ($liftupheight === 120.4){
+
+        echo ($ferrari->liftdown($height,$speed));
+    }
+
 ?>
